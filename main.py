e789636b0d55bb981a5f0d6c97de3c0dab7cb2da
Filippo Leone @North <fleonen@gmail.com>
2019-06-07 11:43:32 +0200

    Added various features and first skeleton for sendemail & json data

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+# coding: utf8
 from bs4 import BeautifulSoup
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+import smtplib
+
+
+def sendmail():
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        # compose and send the email
+
+def prior_check():
+        try:
+                with open('serverlog.json', 'r', encoding='utf8') as serverlog:
+                        jsondata = json.load(serverlog)
+                return jsondata
+        except FileExistsError:
+                return False
 
 if __name__ == '__main__':
         firefoxpath = r'C:\Users\Home\Desktop\kimsufi-scraper\Lib\site-packages\selenium\webdriver\firefox\geckodriver.exe'
@@ -9,30 +30,45 @@ if __name__ == '__main__':
         options.add_argument('-headless')
         driver = Firefox(executable_path=firefoxpath, options=options)
         driver.get('http://www.kimsufi.com/en/servers.xml')
-        html_doc = driver.page_source
-        driver.quit()
-        
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        tables = soup.findAll('table', {'class' : 'full homepage-table'})
+        time.sleep(5)
+        status = driver.execute_script('function a() {return document.readyState;};return a();')
+        if status == 'complete':
+                html_doc = driver.page_source
+                driver.quit()
+                soup = BeautifulSoup(html_doc, 'html.parser')
+                tables = soup.findAll('table', {'class' : 'full homepage-table'})
+                serverdict = {}
 
-        for index,table in enumerate(tables):
-                if index == 0:
-                        english_servers = table.findAll('tr', {'class' : 'zone-dedicated-availability'})
-                else:
-                        canadian_servers = table.findAll('tr', {'class' : 'zone-dedicated-availability'})
+                for index,table in enumerate(tables):
+                        if index == 0:
+                                english_servers = table.findAll('tr', {'class' : 'zone-dedicated-availability'})
+                        else:
+                                canadian_servers = table.findAll('tr', {'class' : 'zone-dedicated-availability'})
 
-        for server in english_servers:
-                cart_elements = server.findAll('td', {'class' : 'btn-order-ks2014'})
-                for cart_element in cart_elements:
-                        try:
-                                cart_element['style'];print('English: Server not available')
-                        except KeyError:
-                                print('English: Server available')
+                for server in english_servers:
+                        #serverinfo = server 
+                        serverinfo = 'English: '+''.join(server.text.replace('\n','').replace(' ','').replace('ex.VATCurrentlybeingreplenishedHaveyouthoughtaboutchoosingaSoyouStartserver?Victimofitssuccess!Availablesoon.',''))
+                        cart_elements = server.findAll('td', {'class' : 'btn-order-ks2014'})
+                        for cart_element in cart_elements:
+                                try:
+                                        cart_element['style'];serverdict[serverinfo] = 'Not Available'
+                                except KeyError:
+                                        serverdict[serverinfo] = 'Available'
 
-        for server in canadian_servers:
-                cart_elements = server.findAll('td', {'class' : 'btn-order-ks2014'})
-                for cart_element in cart_elements:
-                        try:
-                                cart_element['style'];print('Canadian: Server not available')
-                        except KeyError:
-                                print('Canadian: Server available')
+                for server in canadian_servers:
+                        serverinfo = 'Canadian: '+''.join(server.text.replace('\n','').replace(' ','').replace('ex.VATCurrentlybeingreplenishedHaveyouthoughtaboutchoosingaSoyouStartserver?Victimofitssuccess!Availablesoon.',''))
+                        cart_elements = server.findAll('td', {'class' : 'btn-order-ks2014'})
+                        for cart_element in cart_elements:
+                                try:
+                                        cart_element['style'];serverdict[serverinfo] = 'Not Available';
+                                except KeyError:
+                                        serverdict[serverinfo] = 'Available';
+        #print(f"{serverdict}")
+        oldjson = prior_check()
+        if oldjson:
+                for (key_old,value_old), (key_new,value_new) in zip(oldjson.items(), serverdict.items()):
+                        if value_old not in value_new:
+                                print(f"{key_new} changed availability and is now: {value_new}.")
+
+        with open('serverlog.json', 'w', encoding='utf8') as jsonlog:
+                jsonlog.write(json.dumps(serverdict, ensure_ascii=False, indent=4))
